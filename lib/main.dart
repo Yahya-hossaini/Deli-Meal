84ae4294daf0b5907abad5c538b0c14ed715953b
Yahya Hossaini <135443317+Yahya-hossaini@users.noreply.github.com>
2024-10-31 15:05:00 +0430

    creating detail screen!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:deli_meals/screens/categories_screen.dart';
 import 'package:deli_meals/screens/category_meals_screen.dart';
+import 'package:deli_meals/screens/meal_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => const CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
     );
   }
